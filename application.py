311c555ad59a4d39946e129b4ffb3b1b7e6ed03e
adamshapiro <adam.shapiro595@gmail.com>
2018-07-22 11:57:56 -0400

    added html and adjusted js and py accordingly

--- application.py
+++ application.py
@@ -1,15 +1,96 @@
-import os
+import os, time
+from datetime import datetime
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 # list of all channels
-channel_list = ['general']
+channel_list = {'general': {'messages': [], 'users': []}}
+
+# list of taken usernames
+usernames = set()
 
 @app.route("/")
 def index():
+
     return render_template("index.html")
+
+@socketio.on("add_user")
+def add_user(username):
+    if username in usernames:
+        return False
+    else:
+        usernames.add(username)
+        return True
+
+@socketio.on("get_channels")
+def get_channels():
+    return [*channel_list]
+
+@socketio.on("add_channel")
+def add_channel(channel):
+    if channel in channel_list:
+        return False
+    else:
+        channel_list[channel] = {'messages': [], 'users': []}
+        emit('new_channel', channel, broadcast=True)
+        return True
+
+@socketio.on("join_channel")
+def join_channel(data):
+    ch = data['channel']
+    if ch in channel_list:
+        channel = channel_list[ch]
+    else:
+        channel = channel_list['general']
+
+    username = data['username']
+    if username is None:
+        username = "NULL"
+
+    if username not in usernames:
+        usernames.add(username)
+    join_room(ch)
+
+    user = next((u for u in channel['users'] if u['name'] == username), None)
+    if user is not None:
+        user['active'] = True
+    else:
+        user = {'name': username, 'active': True}
+        channel['users'].append(user)
+    emit('user_changed', user, room=ch)
+
+    res = {'name': ch, 'channel': channel}
+    return res
+
+@socketio.on("leave")
+def leave(data):
+    channel = data['channel']
+    username = data['username']
+    leave_room(channel)
+
+    user = next(u for u in channel_list[channel]['users'] if u['name'] == username, None)
+    if user is None:
+        return
+    user['active'] = False
+    emit('user_changed', user, room=channel)
+
+@socketio.on("add_message")
+def add_message(data):
+    message = {
+        'user': data['user'],
+        'text': data['message'],
+        'time': datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %I:%M %p")
+    }
+
+    channel = data['channel']
+    messages = channel_list[channel]['messages']
+    messages.append(message)
+    if len(messages) > 100:
+        messages = messages[-100:]
+
+    emit('new_message', message, room=data['channel'])
--- static/index.js
+++ static/index.js
@@ -1,5 +1,6 @@
+var socket = io.connect(`${location.protocol}//${document.domain}:${location.port}`);
+
 document.addEventListener('DOMContentLoaded', () => {
-    const socket = io.connect(`${location.protocol}//${document.domain}:${location.port}`);
 
     socket.on('connect', () => {
         if (!localStorage.getItem('username')) {
@@ -7,35 +8,40 @@ document.addEventListener('DOMContentLoaded', () => {
                 const username = document.getElementById('userModalInput').value;
                 socket.emit('add_user', username, success => {
                     if (success) {
-                        localStorage.setItem(username);
+                        localStorage.setItem('username', username);
                         $('#userModal').modal('hide');
                     } else {
                         document.getElementById('userModalAlert').classList.remove('d-none');
                     }
                 });
             };
-            $('#userModal').modal('show');
+            $('#userModal').modal({
+                backdrop: 'static',
+                keyboard: false
+            });
         }
 
-        socket.emit('get_channels', null, channels => {
+        socket.emit('get_channels', channels => {
             channels.forEach(channel => addChannel(channel));
 
-            if (!localStorage.getItem('channel'))
-                localStorage.setItem('channel', 'general')
+            if (!localStorage.getItem('channel') || localStorage.getItem('channel') == 'undefined')
+                localStorage.setItem('channel', 'general');
 
             joinChannel(localStorage.getItem('channel'));
         });
 
-        document.getElementById('addMessageSend').onclick = () => {
-            const message = document.getElementById('addMessageInput').value;
+        document.getElementById('addMessage').onsubmit = () => {
+            const message = document.getElementById('addMessageInput');
 
             var data = {
                 'user': localStorage.getItem('username'),
-                'message': message,
+                'message': message.value,
                 'channel': localStorage.getItem('channel')
             };
 
             socket.emit('add_message', data);
+            message.value = '';
+            return false;
         };
 
         document.getElementById('channelModalSubmit').onclick = function() {
@@ -45,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (success) {
                     add_modal.value = '';
                     document.getElementById('channelModalAlert').classList.add('d-none');
-                    document.getElementById('addChannel').click();
+                    $('#channelModal').modal('hide');
                     joinChannel(channel_name);
                 }
                 else {
@@ -53,6 +59,21 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             });
         };
+    }); // end of socket.on('connect')
+
+    socket.on('user_changed', user => {
+        addUser(user);
+    });
+
+    socket.on('new_message', message => addMessage(message));
+
+    socket.on('new_channel', channel => addChannel(channel));
+}); // end of document.addEventListener
+
+window.addEventListener('beforeunload', () => {
+    socket.emit('leave', {
+        'username': localStorage.getItem('username'),
+        'channel': localStorage.getItem('channel')
     });
 });
 
@@ -60,45 +81,81 @@ function addChannel (name) {
     const add = document.getElementById('addChannel');
     const button = document.createElement('button');
         button.innerHTML = name;
-        button.className = 'nav-link';
+        button.className = 'nav-link btn btn-dark my-1';
         button.id = name;
-        button.onclick = joinChannel(name);
 
     add.parentNode.insertBefore(button, add);
+    button.onclick = () => { joinChannel(name) };
 }
 
 function joinChannel (channel) {
-    socket.emit('join_channel', channel, messages => {
-        messages.forEach(message => addMessage(message));
-    });
-    localStorage.setItem('channel', channel);
+    var user = localStorage.getItem('username');
 
-    const old = document.querySelector('nav .active');
+    document.getElementById('chatBox').innerHTML = '';
+    document.querySelectorAll('#userBox ul').forEach(ul => ul.innerHTML = '');
+
+    socket.emit('join_channel', {'username': user, 'channel': channel}, res => {
+        name = res['name']
+        req = res['channel']
+        req['users'].forEach(user => addUser(user));
+        req['messages'].forEach(message => addMessage(message));
+        localStorage.setItem('channel', name);
+        document.getElementById('channelName').innerHTML = name;
+        const chatBox = document.getElementById('chatBox');
+        chatBox.scrollTop = chatBox.scrollHeight;
+    });
+
+    const old = document.querySelector('nav .btn-info');
     if (old) {
-        old.classList.remove('active');
-        socket.emit('leave', old.innerHTML);
+        old.classList.replace('btn-info', 'btn-dark');
+        old.classList.remove('border', 'border-danger');
+        socket.emit('leave', {'username': user, 'channel': old.innerHTML});
     }
 
-    document.getElementById(channel).classList.add('active');
+    document.getElementById(channel).classList.replace('btn-dark', 'btn-info');
+    document.getElementById(channel).classList.add('border', 'border-danger');
 }
 
 function addMessage (m) {
     const name = document.createElement('span');
-        name.className = 'float-left font-weight-bold border-right';
+        name.className = 'float-left font-weight-bold border-right border-dark px-2';
         name.innerHTML = m.user;
 
     const text = document.createElement('span');
         text.innerHTML = m.text;
+        text.className = 'px-2'
 
     const time = document.createElement('span');
-        time.className = 'float-right font-italic border-left';
+        time.className = 'float-right font-italic border-left border-dark px-2';
         time.innerHTML = m.time;
 
     const message = document.createElement('li');
-        message.className = 'mr-auto bg-light border border-dark';
+        message.className = 'bg-light border-dark list-group-item';
         message.appendChild(name);
         message.appendChild(text);
         message.appendChild(time);
 
     document.getElementById('chatBox').appendChild(message);
+}
+
+function addUser (user) {
+    if (user['name'] == null || user['active'] == null)
+        return
+
+    var old = document.getElementById(`user-${user['name']}`);
+    if (old != null)
+        old.parentNode.removeChild(old);
+
+    var li = document.createElement('li');
+        li.innerHTML = user['name'];
+        li.className = 'list-group-item';
+        li.id = `user-${user['name']}`
+
+    if (user['active']) {
+        li.classList.add('text-danger');
+        document.getElementById('activeUsers').appendChild(li);
+    } else {
+        li.classList.add('disabled');
+        document.getElementById('inactiveUsers').appendChild(li);
+    }
 }
